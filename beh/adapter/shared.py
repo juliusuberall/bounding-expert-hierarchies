722f2568effbb8ba212f69cb1f41bbd8d91d854a
Julius Überall <juliusueberall@gmail.com>
2025-07-23 15:24:00 +0100

    Add shared adapter functions to remap domains and training data checkpoint

---
+++ beh/adapter/shared.py
@@ -0,0 +1,18 @@
+import jax
+import jax.numpy as jnp
+
+def Remap(value, low1, high1, low2, high2):
+    # Remap values from one domain to another
+    ## Taken from: https://stackoverflow.com/questions/3451553/value-remapping
+    return low2 + (value - low1) * (high2 - low2) / (high1 - low1)
+
+def Checkpoint_Training_Data(x:jax.Array, y:jax.Array):
+    # Checkpoint to catch numeric and type missmatches which are
+    # difficult to debug on lower levels
+    print(f"JAX Accelerator: {jax.devices()[0].device_kind}")
+    print(f"Total Samples: {y.shape}")
+    print(f'X Range: {jnp.min(x)}-{jnp.max(x)} | X Dtype: {x.dtype} | X Type: {type(x)}') 
+    print(f'Y Range: {jnp.min(y)}-{jnp.max(y)} | Y Dtype: {y.dtype} | Y Type: {type(y)}')
+    print(f'X[30] Sample: {x[30]}') 
+    print(f'Y[30] Sample: {y[30]}')
+    pass

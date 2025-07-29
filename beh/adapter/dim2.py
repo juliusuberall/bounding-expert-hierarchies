Fix image intensity normalization and 2D training data checks

beh/adapter/dim2.py
@@ -1,5 +1,4 @@
 import jax.numpy as jnp
-import matplotlib.pyplot as plt
 
 from beh.core.registry import *
 from PIL import Image
@@ -17,7 +16,7 @@ def preprocess_rgba (path : str, reg : CoreRegistry, downsample : bool = True):
         img = img.resize((300, 300), resample=Image.BILINEAR) 
     img = jnp.array(img)
     assert img.shape[1] != 4, f"2D data is expected to have 4 channel but has {img.shape[1]}."
-    y = jnp.array(img[...,3].flatten(), dtype = jnp.float32)
+    y = jnp.array(img[...,3].flatten() / 255, dtype = jnp.float32)
     x = jnp.indices(img[...,3].shape).reshape(2,-1).T.astype(jnp.float32)
 
     # Normalize coordinates to range -1.0 to 1.0

beh/adapter/shared.py
@@ -15,4 +15,17 @@ def checkpoint_training_data(x : jax.Array, y : jax.Array):
     print(f'X[30] Sample: {x[30]}') 
     print(f'Y[30] Sample: {y[30]}')
     print("==================================================")
-    pass
+
+    # Catch wrong data formatting
+    if jnp.min(x) != -1.0:
+        raise ValueError(f"Training data X has not lower bound at -1.0")
+    elif jnp.max(x) != 1.0:
+        raise ValueError(f"Training data X has not upper bound at 1.0")
+    
+    elif jnp.min(y) != 0.0:
+        raise ValueError(f"Training data labels Y have not lower bound at 0.0")
+    elif jnp.max(y) != 1.0:
+        raise ValueError(f"Training data labels Y have not upper bound at 1.0")
+    
+    else:
+        pass
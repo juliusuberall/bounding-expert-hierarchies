c13470b6edef30861459527faba6bc339faef03e
Julius Überall <juliusueberall@gmail.com>
2025-08-08 13:03:16 +0100

    Complete directory registry for all dimensions

--- beh/registry.py
+++ beh/registry.py
@@ -1,31 +1,31 @@
 # Data directory managment
 data_dir_registry = {
     2 : "./data/2D", 
-    3 : None, 
-    4 : None, 
-    10 : None, 
+    3 : "./data/3D", 
+    4 : "./data/4D", 
+    10 : "./data/4D_plus", 
 }
 
 # Model config path managment
 model_config_dir_registry = {
     2 : "./configs/models-2D.yaml", 
-    3 : None, 
-    4 : None, 
-    10 : None, 
+    3 : "./configs/models-3D.yaml", 
+    4 : "./configs/models-4D.yaml", 
+    10 : "./configs/models-4D_plus.yaml",
 }
 
 # Result data directory managment 
 result_dir_registry = {
     2 : "./results/2D", 
-    3 : None, 
-    4 : None, 
-    10 : None, 
+    3 : "./results/3D", 
+    4 : "./results/4D",
+    10 : "./results/4D_plus",
 }
 
 # Model directory managment 
-model_dir_registry = {
-    2 : "./results/2D/models", 
-    3 : None, 
-    4 : None, 
-    10 : None, 
+result_model_dir_registry = {
+    2 : result_dir_registry[2] + "/models", 
+    3 : result_dir_registry[3] + "/models", 
+    4 : result_dir_registry[4] + "/models", 
+    10 : result_dir_registry[10] + "/models", 
 }

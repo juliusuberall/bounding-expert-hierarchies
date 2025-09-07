daa901868f5e1cf4cd2890d74732ca5e01da7d65
Julius Überall <juliusueberall@gmail.com>
2025-09-07 18:24:36 +0200

    Add vdb prep for mlp

--- beh/styler/dim3.py
+++ beh/styler/dim3.py
@@ -4,15 +4,16 @@ import jax.numpy as jnp
 from beh.registry import *
 from beh.core.registry import *
 from beh.core.moe_sparse import *
+from beh.core.mlp import mlp_forward_INF
 from beh.core.shared import batch_data
 from beh.core.moe_benchmarking import gating_confidence
 
 #------------------------------------------------------------------------------------
 
-def prep_moe_openVDB(
+def prep_openVDB(
     dimension : int, 
     vdb_res : int,
-    moe : dict,
+    model ,
     model_key : str,
     configs : dict,
     reg : CoreRegistry):
@@ -22,7 +23,6 @@ def prep_moe_openVDB(
 
     # Get configs
     model_type = configs[model_key]['type']
-    nex = configs[model_key]['nex']
     dim = reg.get(core_keys['data_size_key'])
     bounds = reg.get(core_keys['data_bounds_key'])
 
@@ -34,33 +34,53 @@ def prep_moe_openVDB(
     # Break in batches
     inf_batch_size = 200000 # Hardcoded in project 
     x_batches = batch_data(vdb_x, inf_batch_size)
-
-    # Forward through model and compute voxel densities
-    func = sparse_funcs_200K[model_key]
     ## Trim tail of x that does not fit with batchsize
     x_batched = jnp.stack(x_batches[0:-1])
-    # Originally we used vmap here but this caused for large models always OOM on a T4, 
-    # which is why we swapped to a on devcice sequential compute, forward passing the batch
-    # through each expert.
-    def run_in_batches(func, moe, x_batches):
-        def step(carry, xb):
-            yp = func(moe, xb).flatten()
-            return carry, yp
-        _, yp = jax.lax.scan(step, None, x_batched)
-        return jnp.concatenate(yp, axis=0)
-    yp = run_in_batches(func, moe, x_batched)
-    yp_tail = func(moe, x_batches[-1]).flatten()
-    yp = jnp.concatenate((yp, yp_tail), axis=0)
-    # Remap back in binary range
-    yp = remap(
-        yp, 
-        jnp.min(yp),
-        jnp.max(yp),
-        0,
-        1,)
 
-    # Get top1 expert id per voxel
-    _ , _ , expert_idx = gating_confidence(moe, x_batches)
+    if model_type == 'moe':
+        nex = configs[model_key]['nex']
+        # Forward through model and compute voxel densities
+        func = sparse_funcs_200K[model_key]
+        # Originally we used vmap here but this caused for large models always OOM on a T4, 
+        # which is why we swapped to a on devcice sequential compute, forward passing the batch
+        # through each expert.
+        def run_in_batches(func, moe, x_batches):
+            def step(carry, xb):
+                yp = func(moe, xb).flatten()
+                return carry, yp
+            _, yp = jax.lax.scan(step, None, x_batched)
+            return jnp.concatenate(yp, axis=0)
+        yp = run_in_batches(func, model, x_batched)
+        yp_tail = func(model, x_batches[-1]).flatten()
+        yp = jnp.concatenate((yp, yp_tail), axis=0)
+        # Remap back in binary range
+        yp = remap(
+            yp, 
+            jnp.min(yp),
+            jnp.max(yp),
+            0,
+            1,)
+        # Get top1 expert id per voxel
+        _ , _ , expert_idx = gating_confidence(model, x_batches)
+        # Assign correct material for blender import
+        expert_material_table = {
+            4 : 0,
+            16 : 1,
+            32 : 2,
+        }
+        mat_dix = expert_material_table[nex]
+
+    else:
+        # Forward through mlp
+        func = mlp_forward_INF
+        yp = jax.vmap(lambda X: func(model, X))(x_batched).flatten()
+        ## Add tail
+        x_tail = func(model, x_batches[-1])
+        yp = jnp.concatenate((yp, x_tail.flatten()))
+        # Add placeholder for moe data
+        expert_idx = np.zeros((yp.shape[0]))
+        # Assign correct material for blender import
+        mat_dix = 3
 
     # Format .npz file and export
     activation = np.array(expert_idx.reshape((vdb_res,vdb_res,vdb_res)))
@@ -74,6 +94,6 @@ def prep_moe_openVDB(
                 density = density,
                 origin_offset = origin_offset,
                 voxel_spacing = voxel_spacing,
-                nex = nex)
+                mat_dix = mat_dix)
     print(f"Density range: {float(np.min(density)):.2f} - {float(np.max(density)):.2f}")
     print(f"Exported .npz at {path}")
